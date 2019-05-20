fbc1240ef458fb3befc50bdf3be4c80b8e89f36b
kakatong36 <1742293631@qq.com>
2019-05-20 22:35:20 +0800

    添加瀑布流功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,21 +2,11 @@
   	<div class="hello">
 		 <!-- 导航 -->
 		<div class="container">
-			<!-- <Cascader :data="cascList" size="large"></Cascader> -->
-			<div class='row'>
-				<!-- <div class='col-lg-3 col-sm-6'>
-					<img src="../assets/01.jpg" alt="">
-				</div>
-				<div class='col-lg-3 col-sm-6'>
-					<img src="../assets/02.jpg" alt="">
-				</div>
-				<div class='col-lg-3 col-sm-6'>
-					<img src="../assets/03.jpg" alt="">
-				</div>
-				<div class='col-lg-3 col-sm-6'>
-					<img src="../assets/04.jpg" alt="">
-				</div> -->
+		<div class="waterfall">
+			<div class="item" v-for="item in imgList" :key="item.Id">
+				<img :src="item.ImgUrl">
 			</div>
+		</div>
 		</div>
   	</div>
 </template>
@@ -26,33 +16,48 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-		cascList: [
+		imgList: [
 			{
-				value: 'beijing',
-				label: '北京',
-				children: [
-					{
-						value: 'gugong',
-						label: '故宫'
-					},
-					{
-						value: 'tiantan',
-						label: '天坛'
-					},
-					{
-						value: 'wangfujing',
-						label: '王府井'
-					}
-				]
+				Id: "sssssssssssssssssssssssss",
+				Name: "唯美意境图片",
+				ImgUrl: require("@/assets/01.jpeg")
+			},
+			{
+				Id: "s5555555555555555555ddddddd",
+				Name: "名侦探柯南",
+				ImgUrl: require("@/assets/01.jpg")
+			},
+			{
+				Id: "5dddddddddddddddddddddddd5",
+				Name: "名侦探柯南",
+				ImgUrl: require("@/assets/02.jpg")
+			},
+			{
+				Id: "sssssssssssssssssssss",
+				Name: "唯美意境图片",
+				ImgUrl: require("@/assets/2.jpg")
+			},
+			{
+				Id: "safsfasdgfdfsgdfjkdjdjjd",
+				Name: "唯美意境图片",
+				ImgUrl: require("@/assets/3.jpg")
+			},
+			{
+				Id: "sssssssssssssssslllllllllllll",
+				Name: "唯美意境图片",
+				ImgUrl: require("@/assets/4.jpg")
 			}
-		]
+		],
+		model14: "",
+		loading2: false,
+		options2: []
     }
   },
   methods: {
-
+	  
   },
   created (){
-
+	  window.vm = this;
   }
 }
 </script>
@@ -73,12 +78,37 @@ li {
 a {
   color: #42b983;
 }
-.hello img{
+
+/*大层*/
+.container {
 	width: 100%;
+	padding: 0 10px;
+	margin: 0 auto;
 }
-.container{
-	img{
-		border: 1px solid red;
-	}
+
+/*瀑布流层*/
+.waterfall {
+	-moz-column-count: 2;
+	/* Firefox */
+	-webkit-column-count: 2;
+	/* Safari 和 Chrome */
+	column-count: 2;
+	// 左右图片之间没有间距
+	-moz-column-gap: 0;
+	-webkit-column-gap: 0;
+	column-gap: 0;
+}
+
+/*一个内容层*/
+.item {
+	margin: 0;
+	-moz-page-break-inside: avoid;
+	-webkit-column-break-inside: avoid;
+	break-inside: avoid;
+	border: 1px solid #999;
+}
+
+.item img {
+	width: 100%;
 }
 </style>
